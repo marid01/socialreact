bff91500db4afcfc881985836ebd608d8f354b7f
Marid <maridshikhkerimov@gmail.com>
2021-04-19 16:02:16 +0300

    route exact

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,14 +1,29 @@
+// import React from 'react'
+import { NavLink } from 'react-router-dom'
 import s from './Dialogs.module.css'
 
 function Dialogs(props: any) {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog + ' ' + s.active}>Света</div>
-                <div className={s.dialog}>Игорь</div>
-                <div className={s.dialog}>Иван</div>
-                <div className={s.dialog}>Петя</div>
-                <div className={s.dialog}>Саша</div>
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to="/dialogs/1">Света</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/2">Игорь</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/3">Иван</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/4">Петя</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/5">Саша</NavLink>
+                </div>
+                <div className={s.dialog}>
+                    <NavLink to="/dialogs/6">Виктор</NavLink>
+                </div>
             </div>
             <div className={s.messages}>
                 <div className={s.message}>Hi</div>
@@ -18,7 +33,7 @@ function Dialogs(props: any) {
             </div>
         </div>
 
-)
+    )
 }
 
 export default Dialogs
