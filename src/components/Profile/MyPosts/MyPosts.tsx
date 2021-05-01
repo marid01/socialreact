07d7122a60e17420ea3e5e97f7aadbc347df5752
Marid <maridshikhkerimov@gmail.com>
2021-05-01 15:48:57 +0300

    props for fialogs

---
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -0,0 +1,16 @@
+
+
+function MyPosts(props: any) {
+    return (
+        <div>
+            My posts
+           <div>
+               <textarea></textarea>
+               <button>Add post</button>
+           </div>
+
+        </div>
+    )
+}
+
+export default MyPosts
---
+++ src/components/Profile/Profileinfo/ProfileInfo.tsx
@@ -0,0 +1,16 @@
+
+
+function ProfileInfo(props: any) {
+    return (
+        <div>
+           <div>
+               <img src="https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg"/>
+           </div>
+            <div>
+                ava + description
+            </div>
+        </div>
+    )
+}
+
+export default ProfileInfo
