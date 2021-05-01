71a1b519e983c3b80ba3c13dc7210169eca2c814
Marid <maridshikhkerimov@gmail.com>
2021-05-01 17:16:58 +0300

    map

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -15,7 +15,7 @@ function Message(props: any) {
 }
 
 function Dialogs(props: any) {
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Света'},
         {id: 2, name: 'Игорь'},
         {id: 3, name: 'Иван'},
@@ -23,30 +23,24 @@ function Dialogs(props: any) {
         {id: 5, name: 'Саша'},
         {id: 6, name: 'Виктор'},
     ]
-
-    let messagesData = [
+    let messages = [
         {id: 1, message: 'Hi'},
         {id: 2, message: 'How is your it?'},
         {id: 3, message: 'I\'m fine'},
         {id: 4, message: 'How are you?'},
     ]
 
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id}/>)
+
+    let messagesElements = messages.map(m => <Message message={m.message}/>)
+
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name="Света" id="1"/>
-                <DialogItem name="Игорь" id="2"/>
-                <DialogItem name="Иван" id="3"/>
-                <DialogItem name="Петя" id="4"/>
-                <DialogItem name="Саша" id="5"/>
-                <DialogItem name="Виктор" id="6"/>
-
+                {dialogsElements}
             </div>
             <div className={s.messages}>
-                <Message message={messagesData[0].message} />
-                <Message message={messagesData[1].message} />
-                <Message message={messagesData[2].message} />
-                <Message message={messagesData[3].message} />
+                {messagesElements}
             </div>
         </div>
 
